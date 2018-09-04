ce66e2ce9c34c21d1c0fa8a1176677304645bba6
Anand K <anakrish@microsoft.com>
2018-09-04 11:45:14 -0700

    More changes

--- tests/crypto1/common/tests.cpp
+++ tests/crypto1/common/tests.cpp
@@ -5,29 +5,22 @@
 #include <openenclave/internal/tests.h>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 oe_result_t create_and_read_chain(
-    const char* c1,
-    const char* c2,
-    const char* c3,
+    std::vector<const char*>&& certs,
     oe_cert_chain_t* chain)
 {
     std::string s = "";
-    if (c1)
-        s += c1;
-    if (c2)
-        s += c2;
-    if (c3)
-        s += c3;
+    for(size_t i=0; i < certs.size(); ++i)
+    {
+        if (certs[i])
+            s += certs[i];
+    }
 
     oe_cert_chain_free(chain);
     // size + 1 to include null character.
     oe_result_t result = oe_cert_chain_read_pem(chain, &s[0], s.size() + 1);
-    if (result != OE_OK)
-    {
-        printf("create_chain result = %s\n", oe_result_str(result));
-        fflush(stdout);
-    }
     return result;
 }
 
@@ -38,14 +31,23 @@ void test_cert_chain(
 {
     oe_cert_chain_t chain = {0};
 
-    // Create root, intermediate, leaf chain.
-    // The following passes in host, but fails in enclave.
-    OE_TEST(create_and_read_chain(root, intermediate, leaf, &chain) == OE_OK);
+    // The following order should work.
+    // But it is inverse of open ssl order.
+    OE_TEST(create_and_read_chain(std::vector<const char*>{leaf, intermediate, root}, &chain) == OE_OK);
+
+    OE_TEST(create_and_read_chain(std::vector<const char*>{leaf, NULL, root}, &chain) == OE_FAILURE);
+    OE_TEST(create_and_read_chain(std::vector<const char*>{leaf, intermediate, leaf, intermediate, root}, &chain) == OE_OK);
+
+    OE_TEST(create_and_read_chain(std::vector<const char*>{intermediate, root}, &chain) == OE_OK);
+    OE_TEST(create_and_read_chain(std::vector<const char*>{intermediate, leaf, root}, &chain) == OE_FAILURE);
+    OE_TEST(create_and_read_chain(std::vector<const char*>{intermediate, leaf, intermediate, root}, &chain) == OE_OK);
+
+    OE_TEST(create_and_read_chain(std::vector<const char*>{root, intermediate, leaf}, &chain) == OE_FAILURE);
 
     // The following should fail since the order of certs is different.
     // But passes in host.
-    OE_TEST(create_and_read_chain(leaf, intermediate, root, &chain) == OE_OK);
-    OE_TEST(create_and_read_chain(intermediate, root, leaf, &chain) == OE_OK);
+
+    OE_TEST(create_and_read_chain(std::vector<const char*>{intermediate, root, leaf}, &chain) == OE_FAILURE);
 
     oe_cert_chain_free(&chain);
 }
--- tests/crypto1/host/host.cpp
+++ tests/crypto1/host/host.cpp
@@ -48,7 +48,7 @@ int main(int argc, const char* argv[])
 
     auto root_ca1 = read_file("./data/RootCA1.crt.pem");
     auto intermediate_ca1 = read_file("./data/IntermediateCA1.crt.pem");
-    auto leaf1 = read_file("./data/RootCA1.crt.pem");
+    auto leaf1 = read_file("./data/Leaf2.crt.pem");
     {
         test_cert_chain_args_t args = {
             .root = (char*)&root_ca1[0],
